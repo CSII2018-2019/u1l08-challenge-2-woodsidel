3ef45fa2f662a35e1bc8e903aeddb4602793a853
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2018-10-24 11:40:19 -0400

    Could you look over this as well?

--- src/U1L08ChallengeTwo.java
+++ src/U1L08ChallengeTwo.java
@@ -2,7 +2,18 @@
 public class U1L08ChallengeTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] randomArray = {5,6,9,4,1,3,2,8,10};
+		int[] numbersSet = {1,2,3,4,5,6,7,8,9,10};
+		int[] numbersIn = {};
+		int i = 0;
+		
+		
+		for(i = 0; i<10; i++) {
+			if(randomArray[i] in numbersSet) {
+				numbersIn[i] == numbers
+			}
+		}
+		
 
 	}
 
